af5cfcb576a1e52b93fe0e2f3e31df1e5844f2e6
Irispiresz <162588398+Irispiresz@users.noreply.github.com>
2026-03-26 09:43:05 -0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedacinho de Amor</title>
-    <link rel="stylesheet" href="css_pda/style_pda.css">
     <link rel="stylesheet" href="css_pda/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css_pda/style_pda.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
 </head>
 <body>
 <header>
-    <h1>Pedacinho de Amor</h1>
+   <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Pedacinho de Amor
+    </a>
+</nav>
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
@@ -19,15 +24,13 @@
             <li><a href="salgados.php">Salgados</a></li>
             <li><a href="personalizados.php">Personalizados</a></li>
 
-            <!-- aqui em baixo é o teste para link da agenda, pra ver se funciona -->
-            <button><li><a href="pastaagenda/indexagenda.php">Agenda</a></li></button>
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalLogin">
-                Login
-            </button>      
-   
-            <button><li><a href="cesta.html">Cesta</a></li></button>
-
-            
+            <li><a class="btn btn-primary" href="pastaagenda/indexagenda.php">Agenda</a></li>
+            <li>
+                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalLogin">
+                    <i class="fa-regular fa-circle-user"></i>Login
+                </button>
+            </li>            
+            <li><a class="btn btn-primary" href="cesta.html">Cesta</a></li>
         </ul>
     </nav>
 </header>
