6f13af0186443f555a83f9128e92b7543b04083b
Irispiresz <162588398+Irispiresz@users.noreply.github.com>
2026-03-28 21:51:07 -0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -4,33 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedacinho de Amor</title>
-    <link rel="stylesheet" href="css_pda/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css_pda/style_pda.css">
-    <link rel="stylesheet" href="fontawesome/css/all.min.css">
-</head>
+<link rel="stylesheet" href="/pedacinhodeamor-main/css_pda/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="css_pda/style_pda.css">
+<link rel="stylesheet" href="css_pda/font-awesome/css/all.min.css"></head>
 <body>
 <header>
-   <nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-        Pedacinho de Amor
-    </a>
-</nav>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="sobrenos.php">Sobre Nós</a></li>
-            <li><a href="doces.php">Doces</a></li>
-            <li><a href="salgados.php">Salgados</a></li>
-            <li><a href="personalizados.php">Personalizados</a></li>
 
-            <li><a class="btn btn-primary" href="pastaagenda/indexagenda.php">Agenda</a></li>
-            <li>
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalLogin">
-                    <i class="fa-regular fa-circle-user"></i>Login
-                </button>
-            </li>            
-            <li><a class="btn btn-primary" href="cesta.html">Cesta</a></li>
-        </ul>
-    </nav>
+  <nav>
+    <a class="navbar-brand" href="#">
+      <img src="/pedacinhodeamor-main/imagens/logo.png" width="30">
+      Pedacinho de Amor
+    </a>
+  </nav>
+    
+   <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
+  <div class="container-fluid">
+
+    
+
+    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+      <ul class="navbar-nav ms-auto">
+
+        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="sobrenos.php">Sobre Nós</a></li>
+        <li class="nav-item"><a class="nav-link" href="doces.php">Doces</a></li>
+        <li class="nav-item"><a class="nav-link" href="salgados.php">Salgados</a></li>
+        <li class="nav-item"><a class="nav-link" href="personalizados.php">Personalizados</a></li>
+
+        <li class="nav-item">
+          <a class="btn btn-primary ms-2" href="pastaagenda/indexagenda.php">Agenda</a>
+        </li>
+
+        <li class="nav-item">
+          <button class="btn btn-primary ms-2" data-bs-toggle="modal" data-bs-target="#modalLogin">
+            <i class="fa-regular fa-circle-user"></i> Login
+          </button>
+        </li>
+
+        <li class="nav-item">
+          <a class="btn btn-primary ms-2" href="cesta.html"><i class="fa-solid fa-basket-shopping"></i> Cesta</a>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
 </header>
