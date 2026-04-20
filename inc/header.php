f7acf2307d34b73cddfd579252430e42c68a9c7f
brendasantos <brendalyasantos@gmail.com>
2026-04-20 15:49:09 -0300

    header e nav quase feitos, falta adicionar fontes e efetitos

--- inc/header.php
+++ inc/header.php
@@ -4,33 +4,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedacinho de Amor</title>
-    <link rel="stylesheet" href="css_pda/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css_pda/style_pda.css">
-    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/awesome/all.min.css">
+    <link rel="stylesheet" href="css/style_pda.css">
 </head>
 <body>
-<header>
-   <nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-        Pedacinho de Amor
-    </a>
-</nav>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="sobrenos.php">Sobre Nós</a></li>
-            <li><a href="doces.php">Doces</a></li>
-            <li><a href="salgados.php">Salgados</a></li>
-            <li><a href="personalizados.php">Personalizados</a></li>
+<header class="">
+        <!-- faixa rosa-->
+    <div class="d-flex justify-content-between" style="background-color: #fbb6cc; width: 100%;">
+            <!-- logo-->
+        <div class="d-flex justify-content-center align-items-center" style="background-color: #fbb6cc;">
+            <a class="navbar-brand d-flex justify-content-center col-md-9" href="index.php">
+                <img src="imagens/logo.png" style="max-height: 150px; padding: 20px;" class="d-inline-block align-top img-fluid" alt="">
+            </a>
+        </div>
+        <!-- botoes login e cesta-->
+        <div class=" d-flex justify-content-end" style="color: #5a3522;">
+            <!-- login-->
+            <div class="me-3 d-flex align-items-center text-center">
+                <a href="#">
+                    <div>
+                        <i class="fa-regular fa-circle-user botaonavbar"></i>
+                    </div>
+                    <div>
+                        <p>Login</p>
+                    </div>
+                </a>
+            </div>
+            <!-- cesta-->
+            <div class="me-3 d-flex align-items-center text-center">
+                <a href="#">
+                    <div>
+                        <i class="fa-solid fa-basket-shopping botaonavbar"></i>
+                    </div>
+                    <div>
+                        <p>Cesta</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+        <!-- botoes-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-lightw-100" style="background-color: #5a3522;">
+    
+        <div class="container-fluid justify-content-center">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-underline container-fluid row justify-content-between">
+                    <li class="nav-item col-2">
+                        <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item col-2">
+                        <a class="nav-link" href="sobrenos.php">Sobre nós</a>
+                    </li>
+                    <li class="nav-item col-2">
+                        <a class="nav-link" href="doces.php">Doces</a>
+                    </li>
+                    <li class="nav-item col-2">
+                        <a class="nav-link" href="salgados.php">Salgados</a>
+                    </li>
+                    <li class="nav-item col-2">
+                        <a class="nav-link" href="personalizados.php">Personalizados</a>
+                    </li>
+                
+                </ul>
+            </div>
+        </div>
 
-            <li><a class="btn btn-primary" href="pastaagenda/indexagenda.php">Agenda</a></li>
-            <li>
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalLogin">
-                    <i class="fa-regular fa-circle-user"></i>Login
-                </button>
-            </li>            
-            <li><a class="btn btn-primary" href="cesta.html">Cesta</a></li>
-        </ul>
     </nav>
 </header>
