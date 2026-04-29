fc016f2fcb2c45acb24b2fd4bdec4e714244fa8f
brendasantos <brendalyasantos@gmail.com>
2026-04-29 16:24:48 -0300

    efeitos adicionados, responsivo

--- inc/header.php
+++ inc/header.php
@@ -13,63 +13,49 @@
 <header class="fixed-top">
         <!-- faixa rosa-->
     <div class="d-flex justify-content-between headerfooter">
-            <!-- logo-->
-        <div class="d-flex justify-content-center align-items-center col-md-3">
-            <a class="navbar-brand d-flex justify-content-center col-md-9" href="index.php">
-                <img src="imagens/logo.png" style="max-height: 140px; padding: 20px;" class="d-inline-block align-top img-fluid" alt="Logo">
-            </a>
-        </div>
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-lightw-100 col-md-6" >
-    
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                    <ul class="navbar-nav container-fluid row justify-content-between">                       
-                        <li class="nav-item col-3">
-                            <a class="nav-link" href="sobrenos.php">Sobre nós</a>
-                        </li>
-                        <li class="nav-item col-2">
-                            <a class="nav-link" href="doces.php">Doces</a>
-                        </li>
-                        <li class="nav-item col-3">
-                            <a class="nav-link" href="salgados.php">Salgados</a>
-                        </li>
-                        <li class="nav-item col-4">
-                            <a class="nav-link" href="personalizados.php">Personalizados</a>
-                        </li>                    
-                    </ul>
+
+       <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+
+            <!-- Logo -->
+            <a class="navbar-brand" href="index.php">
+                <img src="imagens/logo.png" class="img-fluid logoheader" alt="">
+            </a>
+
+            <!-- Botão collapse -->
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" 
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars"></i>
+            </button>
+
+            <!-- Menu -->
+            <div class="collapse navbar-collapse justify-content-center mt-3" id="navbarSupportedContent">
+            <ul class="navbar-nav w-100 justify-content-around text-center">
+                <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+                <li class="nav-item"><a class="nav-link" href="sobrenos.php">Sobre nós</a></li>
+                <li class="nav-item"><a class="nav-link" href="doces.php">Doces</a></li>
+                <li class="nav-item"><a class="nav-link" href="salgados.php">Salgados</a></li>
+                <li class="nav-item"><a class="nav-link" href="personalizados.php">Personalizados</a></li>
+            </ul>
+
+            <!-- Login e Cesta -->
+            <div class="d-flex justify-content-center mt-3" style="color: #5a3522;">
+                <div class="me-3 text-center">
+                <a href="#"><i class="fa-regular fa-circle-user botaonavbar"></i><p>Login</p></a>
+                </div>
+                <div class="me-3 text-center">
+                <a href="#"><i class="fa-solid fa-basket-shopping botaonavbar"></i><p>Cesta</p></a>
                 </div>
             </div>
+            </div>
 
-        </nav>
-        <!-- botoes login e cesta-->
-        <div class=" d-flex justify-content-end col-md-2" style="color: #5a3522;">
-            <!-- login-->
-            <div class="me-3 d-flex align-items-center text-center">
-                <a href="#">
-                    <div>
-                        <i class="fa-regular fa-circle-user botaonavbar"></i>
-                    </div>
-                    <div>
-                        <p>Login</p>
-                    </div>
-                </a>
-            </div>
-            <!-- cesta-->
-            <div class="me-3 d-flex align-items-center text-center">
-                <a href="#">
-                    <div>
-                        <i class="fa-solid fa-basket-shopping botaonavbar"></i>
-                    </div>
-                    <div>
-                        <p>Cesta</p>
-                    </div>
-                </a>
-            </div>
         </div>
+        </nav>
+
+
+       
     </div>
         <!-- botoes-->
     
